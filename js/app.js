a1417ce9ec7023fe2415fe362605721fc93c2c03
jenishm05 <jenishmangukiya05@gmail.com>
2024-01-31 14:38:29 +0530

    Done Changes

--- js/app.js
+++ js/app.js
@@ -1,66 +1,73 @@
 function closeDropdownMenu() {
-  $('.btn-secondary').dropdown('toggle');
+	$('.btn-secondary').dropdown('toggle');
 }
 
 $('#sidebar_collapse').click(function () {
 
-  if ($('.sidebar_menu').hasClass('open')) {
-    $(this).css('transform', 'rotate(180deg)')
-    $('.sidebar_menu').removeClass('open');
-    $('#logo img').attr('src', '../images/logo.png');
-    $('#logo').attr('class', 'logo');
-  } else {
-    $('.sidebar_menu').addClass('open');
-    $('#logo img').attr('src', '../images/logo-big.jpeg');
-    $('#logo').attr('class', 'logo_big');
-    $(this).css('transform', 'rotate(0)')
-  }
-}) 
+	if ($('.sidebar_menu').hasClass('open')) {
+		$(this).css('transform', 'rotate(180deg)')
+		$('.sidebar_menu').removeClass('open');
+		$('#logo img').attr('src', '../images/logo.png');
+		$('#logo').attr('class', 'logo');
+	} else {
+		$('.sidebar_menu').addClass('open');
+		$('#logo img').attr('src', '../images/logo-big.jpeg');
+		$('#logo').attr('class', 'logo_big');
+		$(this).css('transform', 'rotate(0)')
+	}
+})
 
 
-$(function() {
+$(function () {
 	const rowsPerPage = 10;
 	const rows = $('#myTable tbody tr');
 	const rowsCount = rows.length;
 	const pageCount = Math.ceil(rowsCount / rowsPerPage); // avoid decimals
 	const numbers = $('#numbers');
-	
+
 	// Generate the pagination.
 	for (var i = 0; i < pageCount; i++) {
-		numbers.append('<li><a href="#">' + (i+1) + '</a></li>');
+		numbers.append('<li><a href="#">' + (i + 1) + '</a></li>');
 	}
-		
+
 	// Mark the first page link as active.
 	$('#numbers li:first-child a').addClass('active');
 
 	// Display the first set of rows.
 	displayRows(1);
-	
+
 	// On pagination click.
-	$('#numbers li a').click(function(e) {
+	$('#numbers li a').click(function (e) {
 		var $this = $(this);
-		
+
 		e.preventDefault();
-		
+
 		// Remove the active class from the links.
 		$('#numbers li a').removeClass('active');
-		
+
 		// Add the active class to the current link.
 		$this.addClass('active');
-		
+
 		// Show the rows corresponding to the clicked page ID.
 		displayRows($this.text());
 	});
-	
+
 	// Function that displays rows for a specific page.
 	function displayRows(index) {
 		var start = (index - 1) * rowsPerPage;
 		var end = start + rowsPerPage;
-		
+
 		// Hide all rows.
 		rows.hide();
-		
+
 		// Show the proper rows for this page.
 		rows.slice(start, end).show();
 	}
 });
+
+
+$('#handleConform').click(function () {
+	$('#beforeConform').addClass('d-none')
+	$('#afterConform').removeClass('d-none')
+	$(this).text('Confirm')
+})
